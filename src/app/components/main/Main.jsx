c11bb7ef9716c555e5b70965eda1da9f22b1e699
IvanIlyich3 <chpechlivanidis@gmail.com>
2021-07-12 15:08:49 +0300

    store access_token, clear URL, modify eslint

--- src/app/components/main/Main.jsx
+++ src/app/components/main/Main.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Login from './Login/Login';
+import { getAccessToken } from '../../utils/spotifyAuth';
 
-const Main = () => (
-  <div>
-    <Login />
-  </div>
-);
+const Main = () => {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getAccessToken();
+    const { access_token } = hash;
+    console.log(
+      '🚀 ~ file: Main.jsx ~ line 10 ~ useEffect ~ hash',
+      access_token,
+    );
+
+    if (access_token) {
+      setToken(access_token);
+      console.log(token);
+    }
+    window.location.hash = '';
+  });
+
+  return (
+    <div>
+      <Login />
+      {token}
+    </div>
+  );
+};
 
 export default Main;
--- src/app/utils/spotifyAuth.js
+++ src/app/utils/spotifyAuth.js
@@ -15,12 +15,12 @@ const scopes = [
 export const getAccessToken = () => {
   return window.location.hash
     .substring(1)
-    .split('$')
-    .reduce((init, item) => {
-      let parts = item.split('=');
-      init[parts[0]] = decodeURIComponent(parts[1]);
+    .split('&')
+    .reduce((initial, item) => {
+      const parts = item.split('=');
+      initial[parts[0]] = decodeURIComponent(parts[1]);
 
-      return init;
+      return initial;
     }, {});
 };
 
