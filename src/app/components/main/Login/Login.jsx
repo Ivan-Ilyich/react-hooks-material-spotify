7b7b45f12ed9c8938610fe4b6efa3b4a1bcbe7ae
IvanIlyich3 <chpechlivanidis@gmail.com>
2021-07-15 15:11:13 +0300

    add styles to Login component

--- src/app/components/main/Login/Login.jsx
+++ src/app/components/main/Login/Login.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { accessUrl } from '../../../utils/spotifyAuth';
 
 const Login = () => (
-  <div>
+  <div className="login__container">
     <img
       src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
       alt="Spotify Logo"
+      className="spotify-logo"
     />
-    <a href={accessUrl}>Log in with Spotify</a>
+    <a href={accessUrl} className="login__button">
+      Log-In with Spotify
+    </a>
   </div>
 );
 
