598b4485e63cce9372bd8a2e40fe7aed5cb50d3f
IvanIlyich3 <chpechlivanidis@gmail.com>
2021-07-16 19:37:55 +0300

    build ViewSongs structure

---
+++ src/app/components/main/Dashboard/Body/MainView/ViewSongs/SongRow/SongRow.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const SongRow = () => <div>I am the song row</div>;
+
+export default SongRow;
--- src/app/components/main/Dashboard/Body/MainView/ViewSongs/ViewSongs.jsx
+++ src/app/components/main/Dashboard/Body/MainView/ViewSongs/ViewSongs.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
-const ViewSongs = () => <div>I am the view songs</div>;
+const ViewSongs = () => (
+  <div className="view__songs__container">
+    <div className="view__songs__icon">
+      <PlayCircleFilledIcon className="viw__songs__shuffle" />
+      <FavoriteIcon fontSize="large" />
+      <MoreHorizIcon />
+    </div>
+
+    {/* <SongRow /> */}
+  </div>
+);
 
 export default ViewSongs;
