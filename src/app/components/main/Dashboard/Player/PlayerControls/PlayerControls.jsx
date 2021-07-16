298211d2011db6a5bdfd2edc44a1c059bedb9321
IvanIlyich3 <chpechlivanidis@gmail.com>
2021-07-16 13:00:06 +0300

    add mui components in PlayerControls

--- src/app/components/main/Dashboard/Player/PlayerControls/PlayerControls.jsx
+++ src/app/components/main/Dashboard/Player/PlayerControls/PlayerControls.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import RepeatIcon from '@material-ui/icons/Repeat';
 
-const PlayerControls = () => {
-  return <div>I am the Players Controls</div>;
-};
-
+const PlayerControls = () => (
+  <div>
+    <ShuffleIcon />
+    <SkipPreviousIcon />
+    <PlayCircleOutlineIcon />
+    <SkipNextIcon />
+    <RepeatIcon />
+  </div>
+);
 export default PlayerControls;
