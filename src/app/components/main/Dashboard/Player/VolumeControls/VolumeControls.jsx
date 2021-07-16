671303149faebf687096af8c74b9041f312c0383
IvanIlyich3 <chpechlivanidis@gmail.com>
2021-07-16 13:29:14 +0300

    build VolumeControls component

--- src/app/components/main/Dashboard/Player/VolumeControls/VolumeControls.jsx
+++ src/app/components/main/Dashboard/Player/VolumeControls/VolumeControls.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Grid, Slider } from '@material-ui/core';
+import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
+import VolumeDownIcon from '@material-ui/icons/VolumeDown';
 
 const VolumeControls = () => {
-  return <div>I am the Volumes controls</div>;
+  return (
+    <Grid container spacing={2}>
+      <Grid item>
+        <PlaylistPlayIcon />
+      </Grid>
+      <Grid item>
+        <VolumeDownIcon />
+      </Grid>
+      <Grid item xs>
+        <Slider aria-labelledby="continuous-slider" />
+      </Grid>
+    </Grid>
+  );
 };
 
 export default VolumeControls;
