0c016030877ade8905f715021cf7d7a4e5ce66d7
IvanIlyich3 <chpechlivanidis@gmail.com>
2021-07-16 20:12:45 +0300

    add styles to ViewSongs and build SongRow basic structure

--- src/app/components/main/Dashboard/Body/MainView/ViewSongs/SongRow/SongRow.jsx
+++ src/app/components/main/Dashboard/Body/MainView/ViewSongs/SongRow/SongRow.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 
-const SongRow = () => <div>I am the song row</div>;
-
+const SongRow = () => (
+  <div className="song__row__container">
+    <img
+      className="song__row__album"
+      src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.scdn.co%2Fimage%2F0998c9cfe10ea8318ec3a1cbec6e63e4a4ecc802&f=1&nofb=1"
+      alt="asdasd"
+    />
+    <div className="song__row__info">
+      <h1>track name</h1>
+      <p>info about the song artist etc..</p>
+    </div>
+  </div>
+);
 export default SongRow;
