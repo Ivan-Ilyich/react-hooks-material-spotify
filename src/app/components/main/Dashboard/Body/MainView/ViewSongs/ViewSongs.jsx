b97643b3db6e381198a4788676b2a9fcb15ed989
IvanIlyich3 <chpechlivanidis@gmail.com>
2021-07-16 20:00:53 +0300

    add styles to ViewSongs

--- src/app/components/main/Dashboard/Body/MainView/ViewSongs/ViewSongs.jsx
+++ src/app/components/main/Dashboard/Body/MainView/ViewSongs/ViewSongs.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import SongRow from './SongRow/SongRow';
 
 const ViewSongs = () => (
   <div className="view__songs__container">
     <div className="view__songs__icon">
-      <PlayCircleFilledIcon className="viw__songs__shuffle" />
+      <PlayCircleFilledIcon className="view__songs__shuffle" />
       <FavoriteIcon fontSize="large" />
       <MoreHorizIcon />
     </div>
 
-    {/* <SongRow /> */}
+    <SongRow />
   </div>
 );
 
