092ab8f5be5cf9b4a8d4b4e7767cd50ea0df149d
IvanIlyich3 <chpechlivanidis@gmail.com>
2021-07-16 13:37:21 +0300

    add styles to PlayerDetails

--- src/app/components/main/Dashboard/Player/PlayerDetails/PlayerDetails.jsx
+++ src/app/components/main/Dashboard/Player/PlayerDetails/PlayerDetails.jsx
@@ -4,11 +4,11 @@ const PlayerDetails = () => {
   return (
     <div className="player__details__container">
       <img
-        className="player__albumLogo"
+        className="player__details__albumLogo"
         src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.scdn.co%2Fimage%2F0998c9cfe10ea8318ec3a1cbec6e63e4a4ecc802&f=1&nofb=1"
         alt="album logo"
       />
-      <div className="player__songInfo">
+      <div className="player__details__songInfo">
         <h4>some song</h4>
         <p>...</p>
       </div>
