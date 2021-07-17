ee2591982eace1d88dcb494a57c4bd7f66eef2f4
IvanIlyich3 <chpechlivanidis@gmail.com>
2021-07-17 15:57:23 +0300

    build types.js

---
+++ src/app/context/consts/types.js
@@ -0,0 +1,8 @@
+export const SET_USER = 'SET_USER';
+export const SET_ACCESS_TOKEN = 'SET_ACCESS_TOKEN';
+export const SET_ID = 'SET_ID';
+export const SET_PLAYLISTS = 'SET_PLAYLISTS';
+export const SET_DISCOVER_WEEKLY = 'SET_DISCOVER_WEEKLY';
+export const SET_TOP_ARTISTS = 'SET_TOP_ARTISTS';
+export const SET_PLAYING = 'SET_PLAYING';
+export const SET_ITEM = 'SET_ITEM';
