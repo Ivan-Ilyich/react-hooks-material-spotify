c438df915291f809d6349c26e64db043ffce04a9
IvanIlyich3 <chpechlivanidis@gmail.com>
2021-07-21 15:09:59 +0300

    select track from SongRow component

--- src/app/components/main/Dashboard/Body/MainView/ViewSongs/SongRow/SongRow.jsx
+++ src/app/components/main/Dashboard/Body/MainView/ViewSongs/SongRow/SongRow.jsx
@@ -1,8 +1,16 @@
+/* eslint-disable no-undef */
 import React from 'react';
 
-const SongRow = ({ name, album, artist, imageUrl, id }) => (
-  <div className="song__row__container" id={id} role="button" tabIndex={0}>
-    <img className="song__row__album" src={imageUrl} alt="asdasd" id={id} />
+const SongRow = ({ name, album, artist, imageUrl, id, handleClick }) => (
+  <div
+    className="song__row__container"
+    id={id}
+    role="button"
+    tabIndex={0}
+    onClick={handleClick}
+    onKeyPress={handleClick}
+  >
+    <img className="song__row__album" src={imageUrl} alt="album logo" id={id} />
     <div className="song__row__info" id={id}>
       <h1 id={id}>{name}</h1>
       <p id={id}>
--- src/app/components/main/Dashboard/Body/MainView/ViewSongs/ViewSongs.jsx
+++ src/app/components/main/Dashboard/Body/MainView/ViewSongs/ViewSongs.jsx
@@ -5,9 +5,24 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import SongRow from './SongRow/SongRow';
 import { useDataLayer } from '../../../../../../context/store';
+import * as types from '../../../../../../context/consts/types';
 
 const ViewSongs = () => {
-  const [{ selectedPlaylistsTracks }] = useDataLayer();
+  const [{ selectedPlaylistsTracks }, dispatch] = useDataLayer();
+
+  const handleClick = (e) => {
+    const selectedTrack = selectedPlaylistsTracks.find(
+      (track) => track.id === e.target.id,
+    );
+    console.log(
+      '🚀 ~ file: ViewSongs.jsx ~ line 20 ~ handleClick ~ selectedTrack',
+      selectedTrack,
+    );
+    dispatch({
+      type: types.SET_SELECTED_TRACK,
+      payload: selectedTrack,
+    });
+  };
 
   return (
     <div className="view__songs__container">
@@ -26,6 +41,7 @@ const ViewSongs = () => {
               album={tr?.album?.name}
               artist={tr?.artists?.[0].name}
               imageUrl={tr?.album?.images?.[1].url}
+              handleClick={handleClick}
             />
           );
         })}
