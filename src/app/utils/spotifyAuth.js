bd2d19732f8827a94dbc53760178ee497f75de9f
IvanIlyich3 <chpechlivanidis@gmail.com>
2021-07-24 18:39:44 +0300

    config netlify.toml

--- src/app/utils/spotifyAuth.js
+++ src/app/utils/spotifyAuth.js
@@ -3,7 +3,7 @@ export const authEndpoint = 'https://accounts.spotify.com/authorize';
 
 // Replace with your app's client ID, redirect URI and desired scopes
 const clientId = '6c7b4840a2754c7599fe1fd09c74c913';
-const redirectUri = 'https://fervent-mccarthy-b9cf5a.netlify.app/';
+const redirectUri = 'http://localhost:3000/';
 const scopes = [
   'user-read-currently-playing',
   'user-read-recently-played',
