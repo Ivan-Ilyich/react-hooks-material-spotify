77750275af61280fd3e7224a8f68aa0b74da941e
IvanIlyich3 <chpechlivanidis@gmail.com>
2021-07-24 19:54:16 +0300

    change sites domain

--- src/app/utils/spotifyAuth.js
+++ src/app/utils/spotifyAuth.js
@@ -3,7 +3,7 @@ export const authEndpoint = 'https://accounts.spotify.com/authorize';
 
 // Replace with your app's client ID, redirect URI and desired scopes
 const clientId = '6c7b4840a2754c7599fe1fd09c74c913';
-const redirectUri = 'https://fervent-mccarthy-b9cf5a.netlify.app/';
+const redirectUri = 'https://natsios-christos-spotify.netlify.app/';
 const scopes = [
   'user-read-currently-playing',
   'user-read-recently-played',
